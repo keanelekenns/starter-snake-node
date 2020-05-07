ebfee77cedf2bcf13e70d75e06255c93300b7eea
keanelekenns <43152879+keanelekenns@users.noreply.github.com>
2020-05-07 00:38:58 -0700

    logging

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ function pathScore(startCoord, move, board, grid){
     let offset;
     for(offset of offsetArray){
         let coord = {x:startCoord.x + offset[0], y: startCoord.y + offset[1]};
-        //console.log(coord.x + " " + coord.y);
+        console.log(coord.x + " " + coord.y);
         if(!(inBounds(coord,board))){
             score += -1/n;
             continue;
